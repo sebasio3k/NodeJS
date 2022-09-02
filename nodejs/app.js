604de0d3f6cbc5f93392c5b12c3bdc3a592ebbab
sebasio3k <bastianhdezo@gmail.com>
2022-09-02 17:18:52 -0500

    routing and redirecting requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,6 @@
-// import:
+// imports:
 const http = require("http");
+const fs = require("fs");
 
 // Funtion that will run for every request:
 // function rqListener(req, res) {}
@@ -11,8 +12,43 @@ const http = require("http");
 // });
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-//   process.exit();
+  //   process.exit();
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body>");
+    res.write("<h1>Node.js Server</h1>");
+    res.write(
+      "<form action='/message' method='POST'><input type='text' name='message'><button type='sumbit'>Send</button></form>"
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+  }
+
+  //Main Page:
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body>");
+  res.write("<h1>Hello from my Node.js Server</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+
+  console.log("req.url ->", req.url);
+  console.log("req.method ->", req.method);
+  console.log("req.headers ->", req.headers);
 });
 
-server.listen(3000);
+server.listen(4000);
