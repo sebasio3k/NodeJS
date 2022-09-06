bf4befb93dd41a9a23f4c546511d9b8590bd6943
sebasio3k <bastianhdezo@gmail.com>
2022-09-06 09:37:43 -0500

    middlewares

---
+++ nodejs/app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const express = require("express");
+
+// Express application object:
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("in the middleware!");
+  next(); // allows the request to continue the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware!");
+  // ...
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
