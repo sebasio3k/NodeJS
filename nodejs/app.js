9d5d357be65251ce892ad04cb1fbee2952ae051d
sebasio3k <bastianhdezo@gmail.com>
2022-09-06 17:56:32 -0500

    FTR: sending files, paths

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -14,7 +16,8 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  // res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, 'views', '404-not-found.html'));
 });
 
 app.listen(3001);
--- nodejs/routes/shop.js
+++ nodejs/routes/shop.js
@@ -1,10 +1,17 @@
+const path = require("path");
+
 const express = require("express");
+
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   console.log("/ middleware!");
   // ...
-  res.send("<h1>Hello from Express!</h1>");
+  // res.send("<h1>Hello from Express!</h1>");
+  // res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
