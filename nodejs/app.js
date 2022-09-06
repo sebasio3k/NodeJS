46c867fbeb4cf51b9916172fd0443b88e165df60
sebasio3k <bastianhdezo@gmail.com>
2022-09-06 16:18:27 -0500

    FTR: filtering routes

--- nodejs/app.js
+++ nodejs/app.js
@@ -9,10 +9,12 @@ const shopRoutes = require("./routes/shop");
 // Parser:
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes, shopRoutes);
+// app.use(adminRoutes, shopRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
 
-app.listen(3000);
+app.listen(3001);
