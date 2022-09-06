f0f1416d58792e7422ac95372e0b6ec4c1ef0734
sebasio3k <bastianhdezo@gmail.com>
2022-09-06 11:06:29 -0500

    FTR parsing incomming requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// Parser:
+app.use(bodyParser.urlencoded());
+
 app.use("/add-product", (req, res, next) => {
-  console.log("in another middleware!");
-  res.send('<h1>The "Add Product" Page</h1>');
+  console.log("form middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("in another middleware!");
+  console.log("/ middleware!");
   // ...
   res.send("<h1>Hello from Express!</h1>");
 });
