0451ab829c8954abcf009ca4a2e75523d0e89df0
sebasio3k <bastianhdezo@gmail.com>
2022-09-06 11:22:02 -0500

    FTR adding express router

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,25 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 // Parser:
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("form middleware!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("/ middleware!");
-  // ...
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes, shopRoutes);
 
 app.listen(3000);
