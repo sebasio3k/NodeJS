3e23983ddd0ec53e7f7f44046aa6d9c2fe8a8ae4
sebasio3k <bastianhdezo@gmail.com>
2022-09-08 10:46:23 -0500

    FTR: serving files statically

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,13 +11,16 @@ const shopRoutes = require("./routes/shop");
 // Parser:
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Serving files statically:
+app.use(express.static(path.join(__dirname, "public")));
+
 // app.use(adminRoutes, shopRoutes);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).send("<h1>Page not found</h1>");
-  res.status(404).sendFile(path.join(__dirname, 'views', '404-not-found.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404-not-found.html"));
 });
 
 app.listen(3001);
